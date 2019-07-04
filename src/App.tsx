598c44030094680372ea9fec4ebdd4627f90d20a
Sidd Rajpoot <siddrajpoot@gmail.com>
2019-07-03 22:51:12 -0700

    index.html meta size changes. Clear canvas button. Single mobile touch now draws. Offset calculations from canvas width/heigh adjustments.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { createRef } from 'react';
+import Clear from './assets/clear.png';
 
 interface AppState {
   width: number;
@@ -34,9 +35,7 @@ class App extends React.Component<{}, AppState> {
   componentDidMount() {
     this.updateWindowDimensions();
     window.addEventListener('resize', this.updateWindowDimensions)
-    // console.log(this.canvas.current.getContext('2d'))
     if (this.canvas.current) {
-      // console.log(this.canvas.current.getContext('2d'));
       this.ctx = this.canvas.current.getContext('2d');
       if (!this.ctx) return;
     }
@@ -47,13 +46,16 @@ class App extends React.Component<{}, AppState> {
   }
 
   updateWindowDimensions = () => {
-    this.setState({ width: window.innerWidth, height: window.innerHeight })
+    this.setState({ width: window.innerWidth * .8, height: window.innerHeight * .8 })
+  }
+
+  clearCanvas = () => {
+    if (!this.ctx || !this.canvas.current) return;
+    this.ctx.clearRect(0, 0, this.canvas.current.width, this.canvas.current.height);
   }
 
   updateHSL = () => {
     this.hue++;
-    // console.log(this.hue)
-
     if (this.hue > 360) this.hue = 0;
   }
 
@@ -66,14 +68,22 @@ class App extends React.Component<{}, AppState> {
   }
 
   handleMouseMove = (event: React.MouseEvent) => {
-    if (!this.isPressing) return
+    if (!this.isPressing || !this.canvas.current) return
+
     const { clientX, clientY, movementX, movementY } = event
     this.draw(clientX, clientY, movementX, movementY);
     this.updateHSL();
-    // console.log('event :', event);
   }
 
   handleTouchStart = (event: React.TouchEvent) => {
+    const { clientX, clientY } = event.touches[0]
+    if (!this.ctx) return;
+    this.draw(clientX, clientY)
+    this.touchMovementX = clientX;
+    this.touchMovementY = clientY;
+  }
+
+  handleTouchEnd = (event: React.TouchEvent) => {
     this.touchMovementX = null;
     this.touchMovementY = null;
   }
@@ -89,16 +99,26 @@ class App extends React.Component<{}, AppState> {
   }
 
   draw = (x: number, y: number, movementX?: number | null, movementY?: number | null) => {
-    if (!this.ctx) return
+    if (!this.canvas.current || !this.ctx) return;
+    const offsetX = x - this.canvas.current.offsetLeft
+    const offsetY = y - this.canvas.current.offsetTop
+
     this.ctx.beginPath();
     this.ctx.lineCap = 'round';
     this.ctx.lineCap = 'round';
     this.ctx.strokeStyle = `hsl(${this.hue}, 100%, 50%)`
     this.ctx.lineWidth = 30;
-    if ((movementX !== null && movementX !== undefined) && (movementY !== null && movementY !== undefined)) {
-      this.ctx.moveTo(x - movementX, y - movementY)
+    if ((movementX !== null && movementX !== undefined) && (movementY !== null && movementY !== undefined) && this.canvas.current) {
+      this.ctx.moveTo(offsetX - movementX, offsetY - movementY)
+    } else {
+      this.ctx.fillStyle = `hsl(${this.hue}, 100%, 50%)`
+      this.ctx.beginPath();
+      this.ctx.arc(offsetX, offsetY, .1, 0, 2 * Math.PI);
+      this.ctx.stroke();
+      this.ctx.fill();
+      return;
     }
-    this.ctx.lineTo(x, y);
+    this.ctx.lineTo(offsetX, offsetY);
     this.ctx.stroke();
   }
 
@@ -106,6 +126,7 @@ class App extends React.Component<{}, AppState> {
     const { width, height } = this.state;
     return (
       <div className="app">
+        <img src={Clear} className="clear" alt="clear" onClick={this.clearCanvas} />
         <canvas
           className="canvas"
           width={width}
@@ -116,6 +137,7 @@ class App extends React.Component<{}, AppState> {
           onMouseMove={this.handleMouseMove}
           onTouchStart={this.handleTouchStart}
           onTouchMove={this.handleTouchMove}
+          onTouchEnd={this.handleTouchEnd}
         >
         </canvas>
       </div>
